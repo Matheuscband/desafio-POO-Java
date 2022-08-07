0bd283ffc8936ae83911e15f06f48c780eb7f5e0
Perkles <matheuscband@gmail.com>
2022-08-07 13:51:47 -0300

    Final

--- src/main.java
+++ src/main.java
@@ -1,4 +1,6 @@
+import br.com.dio.dominio.Bootcamp;
 import br.com.dio.dominio.Curso;
+import br.com.dio.dominio.Dev;
 import br.com.dio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -15,8 +17,39 @@ public class main {
         mentoria.setDescricao("Desafio inicial de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(mentoria);
+        /*System.out.println(curso1);*/
+        /*System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Curso para se aprimorar em Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMatheus = new Dev();
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devMatheus.getConteudosInscritos());
+        devMatheus.progredir();
+        devMatheus.progredir();
+        devMatheus.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos" + devMatheus.getConteudosConcluidos());
+        System.out.println("XP" + devMatheus.calcularTotalXp());
+
+        System.out.println("------------");
+
+        Dev devAmilton = new Dev();
+        devAmilton.setNome("Amilton");
+        devAmilton.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devAmilton.getConteudosInscritos());
+        devAmilton.progredir();
+        devMatheus.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos" + devAmilton.getConteudosConcluidos());
+        System.out.println("XP" + devAmilton.calcularTotalXp());
+
+
 
     }
 }
